29e0cd5f46ba345f5a88d4b82e39bcde23f82aa3
Supran Sengupta <supransengupta@gmail.com>
2020-08-03 14:07:23 +0530

    updates

--- build/index.js
+++ build/index.js
@@ -10,6 +10,7 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+/** Add all exported modules here */
 __exportStar(require("./errors/BadRequestError"), exports);
 __exportStar(require("./errors/custom-error"), exports);
 __exportStar(require("./errors/database-connection-error"), exports);
