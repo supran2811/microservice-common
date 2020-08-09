eff04645f933f8ba476a814c29bfc8e1426fb570
Supran Sengupta <supransengupta@gmail.com>
2020-08-09 12:32:52 +0530

    updated

---
+++ build/events/base-listener.d.ts
@@ -0,0 +1,29 @@
+import nats, { Message, Stan } from "node-nats-streaming";
+import { Subjects } from "./subjects";
+interface Event {
+    subject: Subjects;
+    data: any;
+}
+export declare abstract class Listener<T extends Event> {
+    abstract subject: T["subject"];
+    abstract queueGroupName: string;
+    abstract onMessage(data: T["data"], msg: Message): void;
+    private client;
+    protected ackWait: number;
+    constructor(client: Stan);
+    /** Enable this option for manual ack
+     *
+     * setDeliverAllAvailable()
+     * It delivers all messages when service goes up first time
+     *
+     * setDurableName
+     * nats will store the messages which
+     * are not acked so that it can redliver
+     * them again when service is up.
+     *
+     */
+    subscriptionOptions(): nats.SubscriptionOptions;
+    listen(): void;
+    parseMessage(msg: Message): any;
+}
+export {};
---
+++ build/events/subjects.d.ts
@@ -0,0 +1,5 @@
+/** Declares channels used by NATS */
+export declare enum Subjects {
+    TicketCreated = "ticket:created",
+    TicketUpdated = "ticket:updated"
+}
